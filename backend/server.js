eae0627747816d8447d0f10b064918ea935716ea
bhupesh jha <shrutijha0803@gmail.com>
2026-01-14 20:04:51 +0530

    backend url  update

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration - allow frontend origin
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173"||"https://staynear-app-backend.onrender.com",
   credentials: true,
 };
 
